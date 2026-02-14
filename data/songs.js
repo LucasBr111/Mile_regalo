f046e6dcc4493d080918932bed00947438344ba4
LucasBr111 <lucasandresbritezalvarez@gmail.com>
2026-02-13 23:41:16 -0300

    Primer commit - estructura base proyecto regalo

---
+++ data/songs.js
@@ -0,0 +1,101 @@
+const songs = [
+  {
+    number: 1,
+    title: "Someone To Spend Time With",
+    artist: "Los Retros",
+    background: "./songs/s1.gif",
+    icons: "💖",
+    lyrics: "./songs/s1/song1.lrc",
+    audio: "./songs/s1/Los Retros - Someone To Spend Time With.mp3"
+  },
+  {
+    number: 2,
+    title: "Lover Is a Day",
+    artist: "CUCO",
+    background: "./songs/s2.gif",
+    icons: "💖",
+    lyrics: "./songs/s2/song2.lrc",
+    audio: "./songs/s2/CUCO - Lover Is a Day (Audio).mp3"
+  },
+  {
+    number: 3,
+    title: "Piel Canela",
+    artist: "Cuco",
+    background: "./songs/s3.gif",
+    icons: "💖",
+    lyrics: "./songs/s3/song3.lrc",
+    audio: "./songs/s3/Cuco - Piel Canela (Audio).mp3"
+  },
+  {
+    number: 4,
+    title: "Decir Adios",
+    artist: "Cuco (ft. DannyLux)",
+    background: "./songs/s4.gif",
+    icons: "💖",
+    lyrics: "./songs/s4/song4.lrc",
+    audio: "./songs/s4/Cuco - Decir Adios (with DannyLux) [Official Visualizer].mp3"
+  },
+  {
+    number: 5,
+    title: "Bossa No Sé",
+    artist: "Cuco (ft. Jean Carter)",
+    background: "./songs/s5.gif",
+    icons: "💖",
+    lyrics: "./songs/s5/song5.lrc",
+    audio: "./songs/s5/Cuco - Bossa No Sé (Lyric Video) ft. Jean Carter.mp3"
+  },
+  {
+    number: 6,
+    title: "Doma",
+    artist: "Josean Log",
+    background: "./songs/s6.gif",
+    icons: "💖",
+    lyrics: "./songs/s6/song6.lrc",
+    audio: "./songs/s6/Josean Log - Doma (Lyric Video).mp3"
+  },
+  {
+    number: 7,
+    title: "this is what falling in love feels like",
+    artist: "JVKE",
+    background: "./songs/s7.gif",
+    icons: "💖",
+    lyrics: "./songs/s7/song7.lrc",
+    audio: "./songs/s7/JVKE - this is what falling in love feels like (Lyrics).mp3"
+  },
+  {
+    number: 8,
+    title: "Chachachá",
+    artist: "Josean Log",
+    background: "./songs/s8.gif",
+    icons: "💖",
+    lyrics: "./songs/s8/song8.lrc",
+    audio: "./songs/s8/Josean Log - Chachachá (Lyric Video).mp3"
+  },
+  {
+    number: 9,
+    title: "Alguien Como Tú",
+    artist: "Jósean Log",
+    background: "./songs/s9.gif",
+    icons: "💖",
+    lyrics: "./songs/s9/song9.lrc",
+    audio: "./songs/s9/Jósean Log - Alguien Como Tú.mp3"
+  },
+  {
+    number: 10,
+    title: "This Side of Paradise",
+    artist: "Coyote Theory",
+    background: "./songs/s10.gif",
+    icons: "💖",
+    lyrics: "./songs/s10/song10.lrc",
+    audio: "./songs/s10/Coyote Theory - This Side of Paradise (Lyric Video).mp3"
+  },
+  {
+    number: 11,
+    title: "Here With Me",
+    artist: "d4vd",
+    background: "./songs/s11.gif",
+    icons: "💖",
+    lyrics: "./songs/s11/song11.lrc",
+    audio: "./songs/s11/d4vd - Here With Me [Official Music Video].mp3"
+  }
+];
